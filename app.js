301b1d5f9f52e6f9ad9ebd83fa6899b332649fd3
Bálint Tóth <kennyka444@gmail.com>
2021-02-27 20:29:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -128,12 +128,11 @@ function changeImg(change, path, containerLen) {
 
 
 function closeImg() {    
-    document.querySelector('.img-btn-next').remove();
-    document.querySelector('.img-btn-prev').remove();
     if (typeof document.querySelector('.img-window') != null) {
+        document.querySelector('.img-btn-next').remove();
+        document.querySelector('.img-btn-prev').remove();
         document.querySelector('.img-window').remove();
-     }
-    
+    }    
 }
 
 
